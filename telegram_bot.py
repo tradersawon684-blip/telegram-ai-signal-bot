1bc87148a36910884fc92485d43b00b341e827a2
tradersawon684-blip <tradersawon684@gmail.com>
2026-07-26 00:22:02 +0600

    telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,12 @@
+import requests
+from config import BOT_TOKEN, CHAT_ID
+
+def send_message(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(url, data=payload, timeout=15)
